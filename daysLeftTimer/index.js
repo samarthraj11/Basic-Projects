c47561cd7b23d56a9e9243d0eff6907da20dc798
Samarth <mailforsamarth@gmail.com>
2021-11-13 22:21:53 +0530

    1

--- daysLeftTimer/index.js
+++ daysLeftTimer/index.js
@@ -5,18 +5,11 @@ function timer()
 {
   const newYearDate = new Date(lastdate);
    const currentDate = new Date();
-
-  //  const secRemaining = (newYearDate - currentDate)/1000;
   const tsec = (Math.floor((newYearDate - currentDate)/1000));
-  // console.log(tsec);
     const tdays = Math.floor(tsec/(3600*24));
   document.getElementById("secondss").innerHTML=tsec;
-  //console.log(tdays);
   const thours = Math.floor(tsec/3600) % 24;
-  //console.log(thours);
   const tmin = Math.floor(tsec/60) % 60;
-  // console.log(tmin);
-  // console.log(tsec%60);
   document.getElementById("minss").innerHTML=tmin;
   document.getElementById("hourss").innerHTML=thours;
   document.getElementById("dayss").innerHTML=tdays;
@@ -25,8 +18,5 @@ function timer()
 }
 
 timer();
-
-// console.log(~~(4/3));
-
  setInterval(timer,1000);
 
